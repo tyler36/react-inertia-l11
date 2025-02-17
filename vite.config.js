e35894175774edd1fe480bfea2f092881ce5ded4
tyler36 <7234392+tyler36@users.noreply.github.com>
2025-02-17 10:02:53 +0900

    ⚙️ ddev-vite support

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+const port = 5173;
+const origin = `${process.env.DDEV_PRIMARY_URL}:${port}`;
+
 export default defineConfig({
     plugins: [
         laravel({
@@ -8,4 +11,14 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    // Adjust Vites dev server for DDEV: https://vitejs.dev/config/server-options.html
+    server: {
+        // The following line is require until the release of https://github.com/vitejs/vite/pull/19241
+        cors: { origin: process.env.DDEV_PRIMARY_URL },
+        // ----------------
+        host: '0.0.0.0',
+        port: port,
+        origin: origin,
+        strictPort: true
+    },
 });
